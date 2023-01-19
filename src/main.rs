b0dc5714dda3ebe0266062b66a7e5375836b0a8d
Giri Priyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2023-01-19 13:25:29 +0000

    rough work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{self, Parser};
+use std::fs;
+use std::io;
+use std::io::prelude::*;
+use std::path::Path;
+use std::time::Instant;
+use zip::{CompressionMethod, ZipWriter};
+
+#[derive(Parser, Default, Debug)]
+#[command(author, version, about, long_about = None)] // Read from `Cargo.toml`
+struct Arguments {
+    #[clap(short, long, default_value = ".")]
+    path: String,
+
+    #[clap(short, long, default_value = "0")]
+    level: u8,
+}
+
+#[tokio::main]
+async fn main() {
+    let started = Instant::now();
+
+    let args = Arguments::parse();
+
+    let zip_path = Path::new("./output/test.zip");
+    let file = fs::File::create(&zip_path).unwrap();
+    let mut zip = ZipWriter::new(file);
+    let options =
+        zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Deflated);
+
+    let folders = vec![Path::new("./test-input/folder1"), Path::new("./test-input/folder2")];
+
+    for folder in folders {
+        for entry in fs::read_dir(folder).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if path.is_dir() {
+                zip.add_directory(&path.to_str().unwrap().to_string(), options)
+                    .unwrap();
+            } else {
+                zip.start_file(
+                    path.file_name().unwrap().to_str().unwrap().to_string(),
+                    options,
+                )
+                .unwrap();
+                let mut file = fs::File::open(&path).unwrap();
+                let mut buffer = Vec::new();
+                file.read_to_end(&mut buffer).unwrap();
+                zip.write_all(&*buffer).unwrap();
+            }
+        }
+    }
+    zip.finish().unwrap();
 }
